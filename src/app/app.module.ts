768719744e94a352700cda0f3fa71f36d25ba2bc
Shehanka <hcsperera@outlook.com>
2019-12-19 18:53:14 +0530

    Team component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { TeamComponent } from './components/common/team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
