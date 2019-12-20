04302e90b6709b825eccc2841ed72f1394c5345c
Shehanka <hcsperera@outlook.com>
2019-12-20 09:04:57 +0530

    navbar separated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { TeamComponent } from './components/common/team/team.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    TeamComponent
+    TeamComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
