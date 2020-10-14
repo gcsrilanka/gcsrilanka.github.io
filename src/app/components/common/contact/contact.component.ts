de64d901bd5153f2dc2cec311e3868f5bbc5aa13
ebonynon <sithumsandeepap@gmail.com>
2020-10-14 08:35:28 +0530

    set emailjs boilerplate

--- src/app/components/common/contact/contact.component.ts
+++ src/app/components/common/contact/contact.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import emailjs, { EmailJSResponseStatus } from "emailjs-com";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"],
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        e.target as HTMLFormElement,
+        "YOUR_USER_ID"
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   }
-
 }
