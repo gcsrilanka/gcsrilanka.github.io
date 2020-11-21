33ae54f0f22d7924cc231ca8f4d2e9b258c36280
Shehanka <hcsperera@outlook.com>
2020-11-22 04:58:10 +0530

    Updated to Angular latest

--- src/app/components/common/promo/promo.component.spec.ts
+++ src/app/components/common/promo/promo.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PromoComponent } from './promo.component';
 
@@ -6,7 +6,7 @@ describe('PromoComponent', () => {
   let component: PromoComponent;
   let fixture: ComponentFixture<PromoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PromoComponent ]
     })
--- src/app/components/common/testimonial/testimonial.component.spec.ts
+++ src/app/components/common/testimonial/testimonial.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TestimonialComponent } from './testimonial.component';
 
@@ -6,7 +6,7 @@ describe('TestimonialComponent', () => {
   let component: TestimonialComponent;
   let fixture: ComponentFixture<TestimonialComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TestimonialComponent ]
     })
