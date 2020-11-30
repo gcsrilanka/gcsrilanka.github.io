b47b321c71a0cb51b7f374887b6d5e96bf2950a0
Shehanka <hcsperera@outlook.com>
2020-11-30 20:00:07 +0530

    New Logos added

--- src/app/components/common/feature/feature.component.ts
+++ src/app/components/common/feature/feature.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Expert } from "src/app/models/expert.model";
+import ExpertsData from "../../../../assets/data/experts.json";
 
 @Component({
-  selector: 'app-feature',
-  templateUrl: './feature.component.html',
-  styleUrls: ['./feature.component.css']
+  selector: "app-feature",
+  templateUrl: "./feature.component.html",
+  styleUrls: ["./feature.component.css"],
 })
 export class FeatureComponent implements OnInit {
+  experts: Expert[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.experts = ExpertsData.experts as Expert[];
   }
 
+  ngOnInit(): void {}
+
+  get expertsCount(): number {
+    return this.experts.length;
+  }
 }
