b5bce2dd14ee132d6f7a14e2519269d159244e3c
shanjathurshan <jathurshanm3@gmail.com>
2021-10-18 17:10:28 +0530

    Fixed #37 - Form doesnot validate properly #37

--- src/app/components/common/contact/contact.component.ts
+++ src/app/components/common/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import emailjs, { EmailJSResponseStatus } from "emailjs-com";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: "app-contact",
@@ -7,26 +9,51 @@ import emailjs, { EmailJSResponseStatus } from "emailjs-com";
   styleUrls: ["./contact.component.css"],
 })
 export class ContactComponent implements OnInit {
-  constructor() {}
+  registerForm!: FormGroup;
+  submitted = false;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder,  private toastr: ToastrService) { }
 
-  public sendEmail(e: Event) {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_664o6wc",
-        "template_sqhcmau",
-        e.target as HTMLFormElement,
-        "user_WyMiG19mwb6vTGJwqJq5C"
-      )
-      .then(
-        (result: EmailJSResponseStatus) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
   }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit(e: Event){
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    } 
+    emailjs.sendForm(
+      "service_664o6wc",
+      "template_sqhcmau",
+      e.target as HTMLFormElement,
+      "user_WyMiG19mwb6vTGJwqJq5C"
+    )
+    .then(
+      (result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        this.onReset();
+      },
+      (error) => {
+        console.log(error.text);
+        this.toastr.error('Try again', 'Sorry!' );
+        e.preventDefault();
+      }
+    );
+    this.toastr.success('submitted successfully', 'Thank you!' );
+    e.preventDefault();
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+}
+
 }
